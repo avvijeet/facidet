7c65a7d93a86840e0196a4f855ba50d3aa471238
Avijeet Diwaker <avvijeet@gmail.com>
2021-07-21 04:14:26 +0530

    moved backend to facidet-backend repo

--- backend/src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-import { image } from "./image";
-import { register } from "./register";
-import { profile } from "./profile";
-import { root } from "./root";
-import { signIn } from "./signIn";
-import { clarifai } from "./clarifai"
-
-export { clarifai, image, profile, register, root, signIn };
